632545c90f85bdbf5d3eba29e0ee57a1dacce1d7
Anmol Bhattarai <anmol.bhattarai18@gmail.com>
2022-08-03 09:42:26 +1000

    added the userSlice and added it to the store

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { configureStore } from "@reduxjs/toolkit";
-import transactionReducer from "./pages/transaction/transactionSlice.js";
 
+import transactionReducer from "./pages/transaction/transactionSlice.js";
+import userReducer from "./pages/userState/userSlice";
 const store = configureStore({
   reducer: {
     transactions: transactionReducer,
+    user: userReducer,
   },
 });
 
